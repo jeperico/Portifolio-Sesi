e72905f103ffbd68b605be25aa8231e98aa91f5d
jeperico <jeperico@terra.com.br>
2024-02-18 12:31:12 -0300

    [fix] fix header bugs

--- perico-src/scripts/HeaderHideable.js
+++ perico-src/scripts/HeaderHideable.js
@@ -1,28 +1,30 @@
 let preScroll = 0;
-let contador = 0;
+let counter = 0;
 window.addEventListener('scroll', () => {
     const header = (document.querySelector(".hideable-header")).classList;
-    
+
     let thisScroll = window.scrollY;
-    
-    if(contador == 10) {
-        contador = 0;
+
+    if (counter == 50) {
+        counter = 0;
         preScroll = thisScroll;
     }
-    
-    if(window.scrollY > 50) {
+
+    if (window.scrollY <= 50) {
+        header.remove('scrollHide');
+        return;
+    } else if (window.scrollY > 50) {
         if (thisScroll === preScroll) return;
         if (thisScroll > preScroll) {
-            if(!header.contains('scrollHide')) {
+            if (!header.contains('scrollHide')) {
                 header.add('scrollHide');
-                console.log(header)
             }
         } else {
-            if(header.contains('scrollHide')) {
+            if (header.contains('scrollHide')) {
                 header.remove('scrollHide');
             }
         }
     }
 
-    contador++;
+    counter++;
 });
