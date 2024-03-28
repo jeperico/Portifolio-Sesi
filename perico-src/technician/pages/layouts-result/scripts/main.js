c1091007a34aed56875979bbec19ca2506e49d01
João Périco <jeperico@terra.com.br>
2024-03-28 09:04:48 -0300

    feat: add stats table

--- perico-src/technician/pages/layouts-result/scripts/main.js
+++ perico-src/technician/pages/layouts-result/scripts/main.js
@@ -17,8 +17,10 @@ const pokeImage = document.querySelector('#pokemon-image');
 const renderPokemon = async (pokemon) => {
     pokeName.innerHTML = 'Loading...';
     const data = await fetchPokemon(pokemon);
-
+    
+    
     if(data) {
+        renderStats(pokemon);
         pokeImage.style.display = 'block';
         pokeName.innerHTML = data.name;
         pokeNumber.innerHTML = data.id;
@@ -27,15 +29,30 @@ const renderPokemon = async (pokemon) => {
         } else {
             pokeImage.src = data.sprites.front_default;
         }   
-        console.log(data.sprites.front_default);
+        // console.log(data.sprites.front_default);
     } else {
         pokeName.innerHTML = 'Not Found Pokemon';
         pokeNumber.innerHTML = '~~';
         pokeImage.style.display = 'none';
     }
-        inputSearch.value = '';
-        searchNum = 1;
-        searchNum = data.id;
+    inputSearch.value = '';
+    searchNum = 1;
+    searchNum = data.id;
+}
+    
+const statusValues = document.querySelectorAll('.stats-value');
+const statusBar = document.querySelectorAll('.status-bar');
+const statusValueTotal = document.querySelector('#status-value-all');
+const renderStats = async (pokemon) => {
+        const data = await fetchPokemon(pokemon);
+        let som = 0;
+        for(const [index, num] of statusValues.entries()) {
+            statusValues[index].innerHTML = data.stats[index].base_stat;
+            statusBar[index].value = data.stats[index].base_stat;
+            som += data.stats[index].base_stat;
+        }
+
+        statusValueTotal.innerHTML = som;
 }
 
 const search = document.querySelector('#form-search');
@@ -62,3 +79,13 @@ nextPoke.addEventListener('click', () => {
 
 
 renderPokemon('1');
+
+// https://pokeapi.co/api/v2/characteristic/{id}/
+
+// id - The identifier for this resource.
+// gene_modulo - The remainder of the highest stat/IV divided by 5.
+// possible_values - The possible values of the highest stat that would result in a Pokémon recieving this characteristic when divided by 5.
+// highest_stat - The stat which results in this characteristic.
+// descriptions - The descriptions of this characteristic listed in different languages.
+
+//
