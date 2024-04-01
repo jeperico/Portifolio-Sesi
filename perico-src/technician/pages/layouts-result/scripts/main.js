5bec06f4a2feef1166e4fb3176feb7bf7ed13e35
João Périco <jeperico@terra.com.br>
2024-04-01 12:48:13 -0300

    feat: add abilities table and inicialize pokedex

--- perico-src/technician/pages/layouts-result/scripts/main.js
+++ perico-src/technician/pages/layouts-result/scripts/main.js
@@ -8,6 +8,15 @@ const fetchPokemon = async (pokemon) => {
     }
 }
 
+const fetchAbility = async (pokemon) => {
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/ability/${pokemon}`);
+    
+    if(APIResponse.status === 200) {
+        const data = await APIResponse.json();   
+        return data;
+    }
+}
+
 let searchNum = 1;
 
 const pokeName = document.querySelector('#pokemon-name');
@@ -17,11 +26,15 @@ const pokeImage = document.querySelector('#pokemon-image');
 const renderPokemon = async (pokemon) => {
     pokeName.innerHTML = 'Loading...';
     const data = await fetchPokemon(pokemon);
+
     
     
     if(data) {
+        abilityContainer.style.display = 'block';
+        descriptionCard.style.display = 'none';
         renderStats(pokemon);
         renderMeasure(pokemon);
+        renderAbilities(pokemon);
         pokeImage.style.display = 'block';
         pokeName.innerHTML = data.name;
         pokeNumber.innerHTML = data.id;
@@ -30,7 +43,6 @@ const renderPokemon = async (pokemon) => {
         } else {
             pokeImage.src = data.sprites.front_default;
         }   
-        // console.log(data.sprites.front_default);
     } else {
         pokeName.innerHTML = 'Not Found Pokemon';
         pokeNumber.innerHTML = '~~';
@@ -56,17 +68,70 @@ const renderStats = async (pokemon) => {
         statusValueTotal.innerHTML = som;
 }
 
-const height = document.querySelector('#poke-weight');
+const height = document.querySelector('#poke-height');
 const weight = document.querySelector('#poke-weight');
 const renderMeasure = async (pokemon) => {
     const data = await fetchPokemon(pokemon);
 
-    height.innerHTML = data.height;
-    weight.innerHTML = data.weight;
+    height.innerHTML = `${(data.height * 10).toFixed(1)}cm`;
+    weight.innerHTML = `${(data.weight * 0.1).toFixed(1)}kg`;
 }
 
 const search = document.querySelector('#form-search');
 const inputSearch = document.querySelector('#input-search');
+
+const defaultAbilities = document.querySelector('#default-abities');
+const hiddeAbilities = document.querySelector('#hidden-abilities-container');
+const displaNone = document.querySelector('#hidden-abilities');
+const abilityContainer = document.querySelector('#ability-container');
+const descriptionCard = document.querySelector('#description-card-abilities');
+const descriptionCardA = document.querySelector('#description-card-text');
+const closeIcon = document.querySelector('#x-icon-asset');
+const a = document.querySelector('#description-ability-title');
+const renderAbilities = async (pokemon) => {
+    const data = await fetchPokemon(pokemon);
+    defaultAbilities.innerHTML = '';
+    hiddeAbilities.innerHTML = '';
+    let isHidde;
+    
+    data.abilities.forEach(async ability => {
+        const abilityData = await fetchAbility((ability.ability.name).toLowerCase());
+        const abilityCard = document.createElement("p");
+        abilityCard.classList.add('poke-card');
+        abilityCard.classList.add('poke-card-ability');
+        abilityCard.innerHTML = ability.ability.name
+        if(!ability.is_hidden) {
+            defaultAbilities.appendChild(abilityCard);
+            isHidde = false;
+        } else {
+            hiddeAbilities.appendChild(abilityCard);
+            isHidde = true;
+        }
+
+        if(!isHidde) {
+            displaNone.style.display = 'none';
+        } else {
+            displaNone.style.display = 'block';
+        }
+        
+        abilityCard.addEventListener('click', () => {
+            descriptionCardA.innerHTML= '';
+            abilityContainer.style.display = 'none';
+            descriptionCard.style.display = 'block';
+            const description = document.createElement("p");
+            
+            description.classList.add('poke-description');
+            a.innerHTML = ability.ability.name;
+            description.innerHTML = abilityData.effect_entries[0].effect;
+            descriptionCardA.appendChild(description);
+        });
+        closeIcon.addEventListener('click', () => {
+            abilityContainer.style.display = 'block';
+            descriptionCard.style.display = 'none';
+        })
+    });
+    
+}
 
 search.addEventListener('submit', (ev) => {
     ev.preventDefault();
@@ -85,7 +150,19 @@ prevPoke.addEventListener('click', () => {
 nextPoke.addEventListener('click', () => {
     searchNum += 1;
     renderPokemon(searchNum);
-})
+});
+
+// const fetchPokemon = async (pokemon) => {
+
+//     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+//     if(APIResponse.status === 200) {
+//         const data = await APIResponse.json();   
+//         return data;
+//     }
+// }
+
+
 
 
 renderPokemon('1');
